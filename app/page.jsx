18dd898cbc3524537c213893f95f08c7d147b4f8
sakib-xrz <sakibxrz21@gmail.com>
2024-08-03 16:18:45 +0600

    Refactor loading component to use Ant Design Spin

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
-import { Loader2 } from "lucide-react";
+import Loading from "@/components/shared/Loading";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
@@ -10,10 +10,5 @@ export default function HomePage() {
   useEffect(() => {
     router.push("/dashboard");
   }, []);
-  return (
-    <div className="text-muted-foreground absolute inset-0 z-10 flex h-screen w-full items-center justify-center gap-2 text-sm">
-      <Loader2 className="h-4 w-4 animate-spin" />
-      Loading...
-    </div>
-  );
+  return <Loading fullscreen={true} />;
 }
--- components/shared/Loading.jsx
+++ components/shared/Loading.jsx
@@ -1,10 +1,9 @@
-import { Loader2 } from "lucide-react";
+import { Spin } from "antd";
 
-export default function Loading() {
+export default function Loading({ fullscreen = false }) {
   return (
-    <div className="absolute inset-0 z-10 flex w-full items-center justify-center gap-2 text-sm text-muted-foreground">
-      <Loader2 className="h-4 w-4 animate-spin" />
-      Loading...
+    <div className="absolute inset-0 z-10 flex w-full items-center justify-center">
+      <Spin fullscreen={fullscreen} tip="Loading" size="large" />
     </div>
   );
 }
